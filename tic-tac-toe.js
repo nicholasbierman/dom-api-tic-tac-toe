68a21b4c764ba0e46af443f129ed53064500d76a
Nick <njb8ah@virginia.edu>
2020-10-02 11:51:41 -0400

    'game status' phase completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,45 @@
 let currentPlayerSymbol = 'x';
-const squareValues = ['', '', '', '', '', '', '', '', '']
+const squareValues = ['', '', '', '', '', '', '', '', ''];
+let gameStatus = '';
+
+function checkGameStatus() {
+     // Check rows
+     for (let i = 0; i < 9; i += 3) {
+          if (squareValues[i] !== ""
+               && squareValues[i] === squareValues [i + 1]
+               && squareValues[i] === squareValues[i + 2]) {
+          gameStatus = squareValues[i];
+          }
+     }
+
+     //Check columns
+     for (let i = 0; i < 3; i++) {
+          if (squareValues[i] !== ''
+               && squareValues[i] === squareValues[i + 3]
+               && squareValues[i] === squareValues[i + 6]) {
+          gameStatus = squareValues[i];
+          }
+     }
+
+     //Check diagonals
+     if (squareValues[0] !== ''
+          && squareValues[0] === squareValues[4]
+          && squareValues[0] === squareValues[8]) {
+     gameStatus = squareValues[0];
+     }
+
+     if (squareValues[2] !== ''
+          && squareVales[2] === squareValues[4]
+          && squareValues[2] === squareValues[6]) {
+     gameStatus = squareValues[2];
+     }
+
+     if (gameStatus !== '') {
+          let gameStatusMessage = document.getElementById('game-status');
+          gameStatusMessage.innerHTML = `Winner: ${gameStatus.toUpperCase()}`;
+     }
+ }
+
 window.addEventListener('DOMContentLoaded', event => {
      const ticTacToeGrid = document.getElementById("tic-tac-toe-board");
      ticTacToeGrid.addEventListener('click', event => {
@@ -22,12 +62,13 @@ window.addEventListener('DOMContentLoaded', event => {
           );
 
           squareValues[index] = currentPlayerSymbol;
-          
+
           currentPlayerSymbol = currentPlayerSymbol === 'x' ? 'o': 'x'
 
           event.target.appendChild(img);
 
 
-
+     checkGameStatus();
      })
+
 })
