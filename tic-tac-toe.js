99e6dc099120c7220432e0ee6c9550cc79017537
Nick <njb8ah@virginia.edu>
2020-10-02 17:44:25 -0400

    finished saveGameState function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,24 +1,69 @@
+const key = 'tic-tac-toe-game-state';
 let currentPlayerSymbol = 'x';
 let squareValues = ['', '', '', '', '', '', '', '', ''];
 let gameStatus = '';
 const newGameButton = document.getElementById('new-game');
 
+function saveGameState() {
+     const state = {
+          currentPlayerSymbol,
+          squareValues,
+          gameStatus,
+     };
+
+     window.localStorage.setItem(key, JSON.stringify(state));
+}
+
+function loadGameState() {
+     const savedState = localStorage.getItem(key);
+
+     if (savedState === null) {
+          return;
+     }
+
+     const state = JSON.parse(savedState);
+
+     currentPlayerSymbol = state.currentPlayerSymbol;
+     squareValues = state.squareValues;
+     gameStatus = state.gameStatus;
+
+     for (let i = 0; i < 9; i++) {
+          if (squareValues[i] !== '') {
+               const img = document.createElement("img");
+
+               img.setAttribute(
+                 "src",
+                 `https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${currentPlayerSymbol}.svg`
+               )
+
+               document.getElementById(`square-${i}`).appendChild(img);
+          }
+     }
+
+     if (gameStatus !== '') {
+          document.getElementById('game-status').innerHTML = `Winner: ${gameStatus}`;
+          newGameButton.disabled = false;
+          document.getElementById('give-up').disabled = true;
+     } else {
+          document.getElementById('game-status').innerHTML = '';
+          newGameButton.disabled = true;
+          document.getElementById('give-up').disabled = false;
+     }
+}
+
 function checkGameStatus() {
   // Check rows
   for (let i = 0; i < 9; i += 3) {
-    if (
-      squareValues[i] !== "" &&
+    if (squareValues[i] !== "" &&
       squareValues[i] === squareValues[i + 1] &&
-      squareValues[i] === squareValues[i + 2]
-    ) {
+      squareValues[i] === squareValues[i + 2]) {
       gameStatus = squareValues[i];
     }
   }
 
   //Check columns
   for (let i = 0; i < 3; i++) {
-    if (
-      squareValues[i] !== "" &&
+    if (squareValues[i] !== "" &&
       squareValues[i] === squareValues[i + 3] &&
       squareValues[i] === squareValues[i + 6]
     ) {
@@ -27,15 +72,14 @@ function checkGameStatus() {
   }
 
   //Check diagonals
-  if (
-    squareValues[0] !== "" &&
+  if (squareValues[0] !== "" &&
     squareValues[0] === squareValues[4] &&
     squareValues[0] === squareValues[8]) {
     gameStatus = squareValues[0];
   }
 
   if (squareValues[2] !== "" &&
-    squareVales[2] === squareValues[4] &&
+    squareValues[2] === squareValues[4] &&
     squareValues[2] === squareValues[6]) {
     gameStatus = squareValues[2];
   }
@@ -57,10 +101,12 @@ function checkGameStatus() {
     let gameStatusMessage = document.getElementById("game-status");
     gameStatusMessage.innerHTML = `Winner: ${gameStatus.toUpperCase()}`;
     newGameButton.disabled = false;
+    giveUpButton.disabled = true;
   }
 }
 
 window.addEventListener('DOMContentLoaded', event => {
+     loadGameState();
      const ticTacToeGrid = document.getElementById("tic-tac-toe-board");
      ticTacToeGrid.addEventListener('click', event => {
           const targetID = event.target.id;
@@ -89,6 +135,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
 
      checkGameStatus();
+     saveGameState();
      });
 
      newGameButton.addEventListener('click', event => {
@@ -107,6 +154,28 @@ window.addEventListener('DOMContentLoaded', event => {
           newGameButton.disabled = true;
           squareValues = ["", "", "", "", "", "", "", "", ""];
 
+          giveUpButton.disabled = false;
+          saveGameState();
+
      });
+
+     const giveUpButton = document.getElementById("give-up");
+
+     giveUpButton.addEventListener('click', event => {
+          if (currentPlayerSymbol === 'x') {
+            gameStatus = "O";
+          } else {
+            gameStatus = "X";
+          }
+
+
+          let gameStatusMessage = document.getElementById("game-status");
+          gameStatusMessage.innerHTML = `Winner: ${gameStatus}`;
+          newGameButton.disabled = false;
+          giveUpButton.disabled = true;
+
+          saveGameState();
+
+     })
 
 })
